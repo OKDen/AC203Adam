0fa91a3eb654170c23f090f6838c6381c1147d41
OKDen <adam.neveux@gmail.com>
2017-06-03 11:45:09 +0800

    Update app.js

--- lesson5,6,7/phaser/app.js
+++ lesson5,6,7/phaser/app.js
@@ -36,8 +36,8 @@ function create(){
 		player.animations.add('left', [0, 1, 2, 3], 10, true);
 		player.animations.add('right', [5, 6, 7, 8], 10, true);
 		game.physics.arcade.enable(player);
-		player.body.bounce.y = 0.7;
-		player.body.gravity.y = 600;
+		player.body.bounce.y = 0.2;
+		player.body.gravity.y = 100;
 		player.body.collideWorldBounds = true;
 
 	enemy1 = game.add.sprite(760, 20, 'baddie');
